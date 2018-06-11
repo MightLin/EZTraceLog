ee3879c376b62c9e8cd19c70494e25bc1bc18f84
MightLin <might0215+github@gmail.com>
2018-06-11 17:09:47 +0800

    add test

---
+++ EZTraceLogTests/TraceLogTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EZTraceLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EZTraceLog.Tests
+{
+	[TestClass()]
+	public class TraceLogTests
+	{
+		[TestMethod()]
+		public void InfoWriteTest()
+		{
+			EZTraceLog.TraceLog.InfoWrite("test", "test message");
+		}
+	}
+}
